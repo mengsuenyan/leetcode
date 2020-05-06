f6f33176a73897ca3bf2628144bbc8839870d41d
mengsuenyan <mengsuenyan@163.com>
2020-05-06 22:04:45 +0800

    [ADD] Search Insert Position, O( log(n) );

--- rust/leetcode/src/lc_arr.rs
+++ rust/leetcode/src/lc_arr.rs
@@ -150,6 +150,21 @@ pub fn climb_stairs(n: i32) -> i32 {
     cur
 }
 
+/// Given a sorted array and a target value, return the index if the target is found. If not, return the index where it would be if it were inserted in order.  
+///   
+/// You may assume no duplicates in the array.  
+///   
+/// 来源：力扣（LeetCode）  
+/// 链接：https://leetcode-cn.com/problems/search-insert-position  
+/// 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。  
+/// O( log(n) )
+pub fn search_insert(nums: Vec<i32>, target: i32) -> i32 {
+    match nums.binary_search(&target) {
+        Ok(x) => x as i32,
+        Err(x) => x as i32,
+    }
+}
+
 #[cfg(test)]
 mod tests {
     #[test]
