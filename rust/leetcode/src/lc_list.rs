1774f706646b73848b4eac2b1591fd6238f08c93
mengsuenyan <mengsuenyan@163.com>
2020-08-20 19:47:37 +0800

    [UPD] 链表相关;

--- rust/leetcode/src/lc_list.rs
+++ rust/leetcode/src/lc_list.rs
@@ -17,8 +17,9 @@ impl ListNode {
             val
         }
     }
+}
 
-
+impl ListNode {
     fn from_vec(v: &Vec<i32>) -> Option<Box<ListNode>> {
         if v.is_empty() {
             return None;
@@ -47,6 +48,15 @@ impl ListNode {
         }
         
         v
+    }
+    
+    fn len(mut head: &Option<Box<ListNode>>) -> usize {
+        let mut l = 0;
+        while let Some(x) = head {
+            l += 1;
+            head = &x.next;
+        }
+        l
     }
 }
 
@@ -254,9 +264,313 @@ pub fn remove_nth_from_end(head: Option<Box<ListNode>>, n: i32) -> Option<Box<Li
     ListNode::from_vec(&v)
 }
 
+/// Add Two Numbers
+///
+/// You are given two linked lists representing two non-negative numbers. e digits are stored in reverse
+/// order and each of their nodes contain a single digit. Add the two numbers and return it as a linked list.
+/// Input: (2 -> 4 -> 3) + (5 -> 6 -> 4)
+/// Output: 7 -> 0 -> 8
+pub fn add_two_numbers(l1: Option<Box<ListNode>>, l2: Option<Box<ListNode>>) -> Option<Box<ListNode>> {
+    let (mut l1, mut l2, mut carry) = (l1.as_ref(), l2.as_ref(), 0);
+    let mut head = Box::new(ListNode::new(0));
+    let mut tail = head.as_mut();
+    
+    loop {
+        let val = match (l1, l2) {
+            (Some(x), Some(y)) => {
+                l1 = x.next.as_ref();
+                l2 = y.next.as_ref();
+                x.val + y.val + carry
+            },
+            (Some(x), None) => {
+                l1 = x.next.as_ref();
+                x.val + carry
+            },
+            (None, Some(y)) => {
+                l2 = y.next.as_ref();
+                y.val + carry
+            }
+            _ => break,
+        };
+        
+        let val = if val > 9 {carry = 1; val - 10} else {carry = 0; val};
+        tail.next = Some(Box::new(ListNode::new(val)));
+        tail = tail.next.as_mut().unwrap();
+    }
+
+    if carry > 0 {
+        tail.next = Some(Box::new(ListNode::new(carry)));
+    }
+    head.next
+}
+
+/// Remove Duplicates from Sorted List II
+/// 
+/// Given a sorted linked list, delete all nodes that have duplicate numbers, leaving only distinct numbers
+/// from the original list.
+/// For example,
+/// Given 1->2->3->3->4->4->5, return 1->2->5.
+/// Given 1->1->1->2->3, return 2->3
+pub fn delete_duplicates_ii(head: Option<Box<ListNode>>) -> Option<Box<ListNode>> {
+    if head.is_none() {
+        return None;
+    }
+    
+    let (mut prev, mut tail) = (head.as_ref().unwrap().val, 
+                                &head.as_ref().unwrap().next);
+    let (mut v, mut is_push) = (vec![prev], true);
+
+    while let Some(x) = tail.as_ref() {
+        let tmp = x.val;
+        if tmp != prev {
+            prev = tmp;
+            is_push = true;
+            v.push(tmp);
+        } else {
+            if is_push {
+                v.pop();
+                is_push = false;
+            }
+        }
+        tail = &x.next;
+    }
+    
+
+    ListNode::from_vec(&v)
+}
+
+/// Rotate List
+/// 
+/// Given a list, rotate the list to the right by k places, where k is non-negative.
+/// For example: Given 1->2->3->4->5->nullptr and k = 2, return 4->5->1->2->3->nullptr.
+pub fn rotate_right(head: Option<Box<ListNode>>, k :i32) -> Option<Box<ListNode>> {
+    if head.is_some() {
+        let mut v = ListNode::to_vec(&head);
+        let k = (k as usize) % v.len();
+
+        v.rotate_right(k);
+        ListNode::from_vec(&v)
+    } else {
+        None
+    }
+}
+
+/// Swap Nodes in Pairs
+/// 
+/// Given a linked list, swap every two adjacent nodes and return its head.
+/// For example, Given 1->2->3->4, you should return the list as 2->1->4->3.
+/// Your algorithm should use only constant space. You may not modify the values in the list, only nodes
+/// itself can be changed
+pub fn swap_pairs(head: Option<Box<ListNode>>) -> Option<Box<ListNode>> {
+    let mut head = head;
+    let mut anchor = ListNode::new(0);
+    let mut tail = &mut anchor;
+    
+    while let Some(mut x) = head {
+        head = x.next.take();
+        if let Some(mut y) = head {
+            head = y.next.take();
+            y.next = Some(x);
+            tail.next = Some(y);
+            tail = tail.next.as_mut().unwrap().next.as_mut().unwrap();
+        } else {
+            tail.next = Some(x);
+            tail = tail.next.as_mut().unwrap();
+        }
+    }
+    
+    anchor.next
+}
+
+/// Linked List Cycle
+/// 
+/// Given a linked list, determine if it has a cycle in it.
+/// Follow up: Can you solve it without using extra space?
+pub fn has_cycle(head: Option<Box<ListNode>>) -> bool {
+    let (mut slow, mut fast) = (head.as_ref(), head.as_ref());
+    while fast.is_some() && fast.unwrap().next.is_some() {
+        slow = slow.unwrap().next.as_ref();
+        fast = fast.unwrap().next.as_ref().unwrap().next.as_ref();
+        if slow == fast {
+            return true;
+        }
+    }
+    false
+}
+
+/// Linked List Cycle II
+/// 
+/// Given a linked list, return the node where the cycle begins. If there is no cycle, return null.
+/// Follow up: Can you solve it without using extra space?
+pub fn detect_cycle(head: Option<Box<ListNode>>) -> Option<Box<ListNode>> {
+    let (mut slow, mut fast) = (head.as_ref(), head.as_ref());
+    
+    while fast.is_some() && fast.unwrap().next.is_some() {
+        slow = slow.unwrap().next.as_ref();
+        fast = fast.unwrap().next.as_ref().unwrap().next.as_ref();
+        if slow == fast {
+            // slow: 相遇点记为x, 假设和slow2可以相遇则走了 (r-x) + n*r;
+            // 此假设成立的条件是slow2走了(r-x) + n*r后在x点;
+            // 记环起点为y, (r-x)+n*r=(L-y-x)+n*r正是x点位置, 故成立;
+            let mut slow2 = head.as_ref();
+            while slow != slow2 {
+                slow = slow.unwrap().next.as_ref();
+                slow2 = slow2.unwrap().next.as_ref();
+            }
+            return Some(Box::new(ListNode::new(slow2.unwrap().val)));
+        }
+    }
+    None
+}
+
+/// Reorder List
+/// 
+/// Given a singly linked list L : L0 -> L1 -> · · · -> Ln−1 -> Ln, reorder it to: L0 -> Ln -> L1 ->
+/// Ln−1 -> L2 -> Ln−2 -> · · ·
+/// You must do this in-place without altering the nodes’ values.
+/// For example, Given {1,2,3,4}, reorder it to {1,4,2,3}
+pub fn reorder_list(head: &mut Option<Box<ListNode>>) {
+    let mut headc = head.clone();
+    let len = ListNode::len(&headc);
+    let (mut half1, mut half2) = (ListNode::new(0), ListNode::new(0));
+    let (mut tail, mut cnt) = (&mut half1, 0);
+    let mut half3 = None;
+    
+    // split
+    let pivot = len >> 1;
+    while let Some(mut x) = headc {
+        headc = x.next.take();
+        if cnt < pivot {
+            tail.next = Some(x);
+            tail = tail.next.as_mut().unwrap();
+        } else if cnt > pivot {
+            x.next = half2.next.take();
+            half2.next = Some(x);
+        } else if (cnt == pivot) && (len & 0x1 == 1) {
+            half3 = Some(x);
+        } else {
+            x.next = half2.next.take();
+            half2.next = Some(x);
+        }
+        cnt += 1;
+    }
+    
+    // merge
+    let mut res = ListNode::new(0);
+    tail = &mut res;
+    let (mut half1, mut half2) = (half1.next, half2.next);
+    while let (Some(mut x), Some(mut y)) = (half1, half2) {
+        half1 = x.next.take();
+        half2 = y.next.take();
+        tail.next = Some(x);
+        tail.next.as_mut().unwrap().next = Some(y);
+        tail = tail.next.as_mut().unwrap().next.as_mut().unwrap();
+    }
+
+    tail.next = half3;
+    
+    *head = res.next;
+}
+
+/**
+ * Your LRUCache object will be instantiated and called as such:
+ * let obj = LRUCache::new(capacity);
+ * let ret_1: i32 = obj.get(key);
+ * obj.put(key, value);
+ */
+/// LRU cache
+/// 
+/// Design and implement a data structure for Least Recently Used (LRU) cache. It should support the
+/// following operations: get and set.
+/// get(key) - Get the value (will always be positive) of the key if the key exists in the cache, otherwise
+/// return -1.
+/// set(key, value) - Set or insert the value if the key is not already present. When the cache reached
+/// its capacity, it should invalidate the least recently used item before inserting a new item
+#[allow(unused)]
+struct LRUCache {
+    // key, (value, slotId)
+    cache: std::collections::HashMap<i32, (i32, usize)>,
+    // slotId, key
+    slot: std::collections::HashMap<usize, i32>,
+    cur_time: usize,
+    old_time: usize,
+    cap: usize,
+}
+
+/**
+ * `&self` means the method takes an immutable reference.
+ * If you need a mutable reference, change it to `&mut self` instead.
+ */
+impl LRUCache {
+
+    #[allow(unused)]
+    fn new(capacity: i32) -> Self {
+        let capacity = capacity as usize;
+        LRUCache {
+            cache: std::collections::HashMap::with_capacity(capacity),
+            slot: std::collections::HashMap::with_capacity(capacity),
+            cur_time: 0,
+            old_time: 0,
+            cap: capacity,
+        }
+    }
+
+    fn update(&mut self, key: i32, time: usize) {
+        self.slot.remove(&time);
+        self.slot.insert(self.cur_time, key);
+        if !self.slot.contains_key(&self.old_time) {
+            self.old_time += 1;
+        }
+    }
+
+    #[allow(unused)]
+    fn get(&mut self, key: i32) -> i32 {
+        match self.cache.get(&key) {
+            Some(&x) => {
+                self.cur_time += 1;
+                self.cache.insert(key, (x.0, self.cur_time));
+                
+                self.update(key, x.1);
+                x.0
+            },
+            None => {-1},
+        }
+    }
+
+    #[allow(unused)]
+    fn put(&mut self, key: i32, value: i32) {
+        self.cur_time += 1;
+        
+        // 时间更新
+        match self.cache.get(&key) {
+            Some(&x) => {
+                self.update(key, x.1);
+            },
+            None => {
+                if self.cache.len() < self.cap {
+                    self.slot.insert(self.cur_time, key);
+                } else {
+                    'out: loop {
+                        while let Some(&x) = self.slot.get(&self.old_time) {
+                            self.slot.remove(&self.old_time);
+                            self.slot.insert(self.cur_time, key);
+                            self.cache.remove(&x);
+                            self.old_time += 1;
+                            break 'out;
+                        }
+                        self.old_time += 1;
+                    }
+                }
+            }
+        }
+
+        self.cache.insert(key, (value, self.cur_time));
+    }
+}
+
 #[cfg(test)]
 mod tests {
-    use crate::lc_list::ListNode;
+    use crate::lc_list::{ListNode, LRUCache};
 
     #[test]
     fn delete_duplicates() {
@@ -298,5 +612,87 @@ mod tests {
             let l1 = ListNode::from_vec(&c.0);
             assert_eq!(super::partition(l1, c.1), ListNode::from_vec(&c.2));
         }
+    }
+    
+    #[test]
+    fn add_two_numbers() {
+        let cases = [
+            ((vec![2,4,3], vec![5,6,4]), vec![7,0,8]),
+            ((vec![9],vec![9]), vec![8,1]),
+        ];
+        cases.iter().for_each(|x| {
+            let tmp = super::add_two_numbers(ListNode::from_vec(&(x.0).0),
+                                             ListNode::from_vec(&(x.0).1));
+            assert_eq!(x.1, ListNode::to_vec(&tmp), "case: {:?}", x.0);
+        })
+    }
+    
+    #[test]    
+    fn delete_duplicates_ii() {
+        let cases = [
+            (vec![1,2,3,3,4,4,5], vec![1,2,5]),
+            (vec![1,1,1,2,3], vec![2,3]),
+        ];
+        
+        cases.iter().for_each(|x| {
+            let tmp = super::delete_duplicates_ii(ListNode::from_vec(&x.0));
+            assert_eq!(x.1, ListNode::to_vec(&tmp), "case: {:?}", x.0);
+        })
+    }
+    
+    #[test]
+    fn rotate_right() {
+
+        let cases = [
+            ((vec![1,2,3,4,5], 2), vec![4,5,1,2,3]),
+            ((vec![0,1,2],4), vec![2,0,1]),
+        ];
+
+        cases.iter().for_each(|x| {
+            let tmp = super::rotate_right(ListNode::from_vec(&(x.0).0), (x.0).1);
+            assert_eq!(x.1, ListNode::to_vec(&tmp), "case: {:?}", x.0);
+        })
+    }
+    
+    #[test]
+    fn swap_pairs() {
+
+        let cases = [
+            (vec![1,2,3,4], vec![2,1,4,3]),
+        ];
+
+        cases.iter().for_each(|x| {
+            let tmp = super::swap_pairs(ListNode::from_vec(&x.0));
+            assert_eq!(x.1, ListNode::to_vec(&tmp), "case: {:?}", x.0);
+        })
+    }
+    
+    #[test]
+    fn reorder_list() {
+
+        let cases = [
+            (vec![1,2,3,4], vec![1,4,2,3]),
+            (vec![1,2,3,4,5], vec![1,5,2,4,3]),
+        ];
+
+        cases.iter().for_each(|x| {
+            let mut tmp = &mut ListNode::from_vec(&x.0);
+            super::reorder_list(&mut tmp);
+            assert_eq!(x.1, ListNode::to_vec(&tmp), "case: {:?}", x.0);
+        })
+    }
+    
+    #[test]
+    fn lru_cache() {
+        let mut cache = LRUCache::new(2);
+        cache.put(1, 1);
+        cache.put(2, 2);
+        assert_eq!(1, cache.get(1));       // 返回  1
+        cache.put(3, 3);    // 该操作会使得关键字 2 作废
+        assert_eq!(-1, cache.get(2));       // 返回 -1 (未找到)
+        cache.put(4, 4);    // 该操作会使得关键字 1 作废
+        assert_eq!(-1, cache.get(1));       // 返回 -1 (未找到)
+        assert_eq!(3, cache.get(3));       // 返回  3
+        assert_eq!(4, cache.get(4));       // 返回  4
     }
 }
