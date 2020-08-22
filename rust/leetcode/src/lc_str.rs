e25723c3cd01f948fb2d7e323cf0fc7ccf6003e0
mengsuenyan <mengsuenyan@163.com>
2020-08-22 20:02:13 +0800

    [UPD] 字符串相关;

--- rust/leetcode/src/lc_str.rs
+++ rust/leetcode/src/lc_str.rs
@@ -1,4 +1,5 @@
 use std::str::Chars;
+use std::iter::Peekable;
 
 pub fn longest_common_prefix(strs: Vec<String>) -> String {
     if strs.is_empty() {
@@ -259,9 +260,323 @@ pub fn longest_palindrome(s: String) -> String {
     }
 }
 
+/// Regular Expression Mating
+///
+/// Implement regular expression matching with support for '.' and '*'.
+/// '.' Matches any single character. '*' Matches zero or more of the preceding element.
+/// e matching should cover the entire input string (not partial).
+/// e function prototype should be:
+/// bool isMatch(const char *s, const char *p)
+/// Some examples:
+/// isMatch("aa","a") → false
+/// isMatch("aa","aa") → true
+/// isMatch("aaa","aa") → false
+/// isMatch("aa", "a*") → true
+/// isMatch("aa", ".*") → true
+/// isMatch("ab", ".*") → true
+/// isMatch("aab", "c*a*b") → true
+pub fn is_match(s: String, p: String) -> bool {
+    
+    is_match_help_(s.chars().peekable(), p.chars().peekable())
+}
+
+fn is_match_help_(mut s: Peekable<Chars>, mut p: Peekable<Chars>) -> bool{
+    if p.peek().is_none() { return s.peek().is_none();}
+
+    let mut pn = p.clone();
+    pn.next();
+    match pn.peek() {
+        Some(&'*') => {
+            let mut pnn = pn.clone();
+            pnn.next();
+
+            // 检查'*'匹配多个
+            while (p.peek() == s.peek()) || (p.peek() == Some(&'.') && s.peek().is_some()) {
+                // 检查'*'匹配0个
+                if is_match_help_(s.clone(), pnn.clone()) {
+                    return true;
+                }
+                s.next();
+            }
+            is_match_help_(s.clone(), pnn)
+        },
+        _ => {
+            // 字符匹配
+            if (p.peek() == s.peek()) || (p.peek() == Some(&'.') && s.peek().is_some()) {
+                s.next();
+                is_match_help_(s.clone(), pn.clone())
+            } else {
+                false
+            }
+        },
+    }
+}
+
+/// Wildcard Mating
+/// 
+/// Implement wildcard paern matching with support for '?' and '*'.
+/// '?' Matches any single character. '*' Matches any sequence of characters (including the empty
+/// sequence).
+/// e matching should cover the entire input string (not partial).
+/// e function prototype should be:
+/// bool isMatch(const char *s, const char *p)
+/// Some examples:
+/// isMatch("aa","a") → false
+/// isMatch("aa","aa") → true
+/// isMatch("aaa","aa") → false
+/// isMatch("aa", "*") → true
+/// isMatch("aa", "a*") → true
+/// isMatch("ab", "?*") → true
+/// isMatch("aab", "c*a*b") → false
+pub fn is_wildcard_match(s: String, p: String) -> bool {
+    let (mut sitr, mut pitr) 
+        = (s.chars().enumerate().skip(0).peekable(), p.chars().enumerate().skip(0).peekable());
+    let mut star = false;
+    let (mut ss, mut ps) = (0, 0);
+    
+    while sitr.peek().is_some() {
+        match pitr.peek() {
+            Some(&(_, '?')) => {
+                sitr.next();
+                pitr.next();
+            },
+            Some(&(_, '*')) => {
+                star = true;
+                while let Some(&(_, '*')) = pitr.peek() { pitr.next(); }
+                if pitr.peek().is_none() { return true;}
+                ss = sitr.peek().unwrap().0;
+                ps = pitr.peek().unwrap().0;
+            },
+            _ => {
+                if pitr.peek().is_none() || sitr.peek().unwrap().1 != pitr.peek().unwrap().1 {
+                    if !star {return false;}
+                    ss += 1;
+                    sitr = s.chars().enumerate().skip(ss).peekable();
+                    pitr = p.chars().enumerate().skip(ps).peekable();
+                } else {
+                    sitr.next();
+                    pitr.next();
+                }
+            }
+        }
+    }
+
+    while let Some(&(_, '*')) = pitr.peek() {pitr.next();}
+    pitr.peek().is_none()
+}
+
+
+/// Valid Number
+/// 
+/// Validate if a given string is numeric.
+/// Some examples:
+/// "0" => true
+/// " 0.1 " => true
+/// "abc" => false
+/// "1 a" => false
+/// "2e10" => true
+/// Note: It is intended for the problem statement to be ambiguous. You should gather all requirements
+/// up front before implementing one
+pub fn is_number(s: String) -> bool {
+    // invalid, space, sign, digit, dot, exponent
+    let ct = [0, 1, 2, 3, 4, 5];
+    let tb = [
+        //i  sp si di do  e
+        [-1, 0, 3, 1, 2, -1], /*initial state*/
+        [-1, 8,-1, 1, 4, 5],  /*digit*/
+        [-1, -1,-1,4,-1,-1],/*dot is occur*/
+        [-1, -1,-1,1, 2,-1],/*sign is occur*/
+        [-1, 8, -1,4,-1, 5],/*float*/
+        [-1, -1, 6,7,-1,-1],/*exponent is occur*/
+        [-1, -1,-1,7,-1,-1],/*sign is occur after exponent*/
+        [-1, 8, -1,7,-1,-1],/*efloat*/
+        [-1, 8, -1,-1,-1,-1],/*final state*/
+    ];
+    
+    let mut st = 0;
+    for ele in s.chars() {
+        let t = if ele == ' ' {
+            ct[1]
+        } else if ele.is_ascii_digit() {
+            ct[3]
+        } else if (ele == '+') || (ele == '-') {
+            ct[2]
+        } else if (ele == 'e') || (ele == 'E') {
+            ct[5]
+        } else if ele == '.' {
+            ct[4]
+        } else {
+            ct[0]
+        };
+        
+        st = tb[st as usize][t];
+        if st == -1 {
+            break;
+        }
+    }
+    
+    st == 1 || st == 4 || st == 7 || st == 8
+}
+
+/// Integer to Roman
+/// 
+/// Given an integer, convert it to a roman numeral.
+/// Input is guaranteed to be within the range from 1 to 3999.
+pub fn int_to_roman(num: i32) -> String {
+    debug_assert!(num >0 && num < 4000, "num must be belong to (0,4000)");
+    let (mut num, mut i) = (num, 0);
+    let radix = [1000, 900, 500, 400, 100, 90, 50, 40, 10, 9, 5, 4, 1];
+    let sym = ["M", "CM", "D", "CD", "C", "XC", "L", "XL", "X", "IX",
+        "V", "IV", "I"];
+    
+    let mut roman = String::with_capacity(128);
+    while num > 0 {
+        let cnt = num / radix[i];
+        num = num % radix[i];
+        (1..=cnt).rev().for_each(|_| { roman.push_str(sym[i])});
+        i += 1;
+    }
+    
+    roman
+}
+
+pub fn roman_to_int(roman: String) -> i32 {
+    let map = |x: char| {
+        match x {
+            'I' => 1,
+            'V' => 5,
+            'X' => 10,
+            'L' => 50,
+            'C' => 100,
+            'D' => 500,
+            'M' => 1000,
+            _ => 0,
+        }
+    };
+    
+    if roman.is_empty() {
+        0
+    } else {
+        let mut res = 0;
+        
+        roman.chars().take(1).for_each(|x| {res += map(x);});
+        roman.chars().skip(1).zip(roman.chars().take(roman.len() - 1)).for_each(|(x, y)| {
+            res += if map(x) > map(y) {
+                map(x) - (map(y) << 1)
+            } else {
+                map(x)
+            };
+        });
+
+        res
+    }
+}
+
+/// Anagrams
+/// 
+/// Given an array of strings, return all groups of strings that are anagrams.
+/// Note: All inputs will be in lower-case.
+pub fn anagrams(strs: Vec<String>) -> Vec<String> {
+    let mut group = std::collections::HashMap::with_capacity(strs.len() >> 1);
+    
+    strs.iter().for_each(|x| {
+        let mut tmp = Vec::with_capacity(x.len());
+        x.chars().for_each(|c| {
+            if c.is_ascii_lowercase() {
+                tmp.push(c as u8);
+            }
+        });
+        tmp.sort();
+        group.entry(tmp).or_insert(Vec::new()).push(x);
+    });
+    
+    let mut res = Vec::new();
+    group.iter().for_each(|x| {
+        if x.1.len() > 1 {
+            x.1.iter().for_each(|&y| {
+                res.push(y.to_string());
+            });
+        }
+    });
+    
+    res
+}
+
+/// Simplify Path
+/// 
+/// Given an absolute path for a file (Unix-style), simplify it.
+/// For example,
+/// path = "/home/", => "/home"
+/// path = "/a/./b/../../c/", => "/c"
+/// Corner Cases:
+/// • Did you consider the case where path = "/../"? In this case, you should return "/".
+/// • Another corner case is the path might contain multiple slashes '/' together, such as
+/// "/home//foo/". In this case, you should ignore redundant slashes and return "/home/foo"
+pub fn simplify_path(path: String) -> String {
+    let mut dirs = Vec::new();
+    
+    let mut itr = path.chars().enumerate().peekable();
+    
+    while itr.peek().is_some() {
+        itr.next();
+        
+        let i = if itr.peek().is_none() {break;} else {itr.peek().unwrap().0};
+        
+        while let Some(&x) = itr.peek() {
+            if x.1 == '/' {
+                break;
+            }
+            itr.next();
+        }
+        
+        let j = if itr.peek().is_none() {path.len()} else {itr.peek().unwrap().0};
+        let dir: String = path.chars().skip(i).take(j - i).collect();
+        if !dir.is_empty() && dir != "." {
+            if dir == ".." {
+                dirs.pop();
+            } else {
+                dirs.push(dir);
+            }
+        }
+    }
+    
+    if dirs.is_empty() {
+        String::from("/")
+    } else {
+        let mut out = String::new();
+        dirs.iter().for_each(|x| {
+            out.push('/');
+            out.push_str(x.as_str());
+        });
+        out
+    }
+}
+
+/// Length of Last Word
+/// 
+/// Given a string s consists of upper/lower-case alphabets and empty space characters ' ', return the
+/// length of last word in the string.
+/// If the last word does not exist, return 0.
+/// Note: A word is defined as a character sequence consists of non-space characters only.
+/// For example, Given s = "Hello World", return 5.
+pub fn length_of_last_word(s: String) -> i32 {
+    let mut res = 0;
+    
+    let s = s.trim_end();
+    for ele in s.chars().rev() {
+        if ele == ' ' {
+            break;
+        } else {
+            res += 1;
+        }
+    }
+    
+    res
+}
+
 #[cfg(test)]
 mod tests {
-    
+
     #[test]
     fn longest_common_prefix() {
         let cases = [
@@ -326,6 +641,85 @@ mod tests {
 
         cases.iter().for_each(|&x| {
             assert_eq!(x.1, super::longest_palindrome(String::from(x.0)), "cases: {:?}", x.0);
+        });
+    }
+    
+    #[test]
+    fn is_match() {
+        let cases = [
+            (("aa", "a*"), true),
+            (("aa","a"), false),
+            (("aa", "aa"), true),
+            (("aaa", "aa"), false),
+            (("aa", ".*"), true),
+            (("ab", ".*"), true),
+            (("aab", "c*a*b"), true),
+        ];
+        cases.iter().for_each(|&x| {
+            assert_eq!(x.1, super::is_match((x.0).0.to_string(), (x.0).1.to_string()), "cases: {:?}", x.0);
+        });
+    }
+    
+    #[test]
+    fn is_wildcard_match() {
+
+        let cases = [
+            (("adceb", "*a*b"), true),
+            (("acdcb","a*c?b"), false),
+            (("", "?"), false),
+            (("aa","a"), false),
+            (("aa", "aa"), true),
+            (("aaa", "aa"), false),
+            (("aa", "*"), true),
+            (("aa", "a*"), true),
+            (("ab", "?*"), true),
+            (("aab", "c*a*b"), false),
+            (("abbabaaabbabbaababbabbbbbabbbabbbabaaaaababababbbabababaabbababaabbbbbbaaaabababbbaabbbbaabbbbababababbaabbaababaabbbababababbbbaaabbbbbabaaaabbababbbbaababaabbababbbbbababbbabaaaaaaaabbbbbaabaaababaaaabb",
+              "**aa*****ba*a*bb**aa*ab****a*aaaaaa***a*aaaa**bbabb*b*b**aaaaaaaaa*a********ba*bbb***a*ba*bb*bb**a*b*bb"), false),
+        ];
+        cases.iter().for_each(|&x| {
+            assert_eq!(x.1, super::is_wildcard_match((x.0).0.to_string(), (x.0).1.to_string()), "cases: {:?}", x.0);
+        });
+    }
+    
+    #[test]
+    fn is_number() {
+
+        let cases = [
+            ("+.8", true),
+            ("0", true),
+            (" 0.1 ", true),
+            ("abc", false),
+            ("1 a", false),
+            ("2e10", true),
+        ];
+        cases.iter().for_each(|&x| {
+            assert_eq!(x.1, super::is_number(x.0.to_string()), "cases: {:?}", x.0);
+        });
+    }
+    
+    #[test]
+    fn simplify_path() {
+
+        let cases = [
+            ("/home/", "/home"),
+            ("/a/./b/../../c/", "/c"),
+            ("/../", "/"),
+            ("/home//foo", ("/home/foo")),
+        ];
+        cases.iter().for_each(|&x| {
+            assert_eq!(x.1, super::simplify_path(x.0.to_string()).as_str(), "cases: {:?}", x.0);
+        });
+    }
+    
+    #[test]
+    fn length_of_last_word() {
+
+        let cases = [
+            ("Hello World", 5),
+        ];
+        cases.iter().for_each(|&x| {
+            assert_eq!(x.1, super::length_of_last_word(x.0.to_string()), "cases: {:?}", x.0);
         });
     }
 }
