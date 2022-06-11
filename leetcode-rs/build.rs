5e87bed5a3a0e5c2872554c89d1c0c7274268964
mengsuenyan <mengsuenyan@163.com>
2022-06-11 17:40:01 +0800

    [ADD] add build script that limit the version of rustc;

---
+++ leetcode-rs/build.rs
@@ -0,0 +1,9 @@
+use rustversion::since;
+
+#[since(1.61.0)]
+#[derive(Debug)]
+pub struct NeedToInstallRustVerGreatThan1_61_0;
+
+fn main() {
+    println!("{:?}", NeedToInstallRustVerGreatThan1_61_0{});
+}
